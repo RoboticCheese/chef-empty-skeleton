ec712493281d6ab9698aba5597e4a5fbbd3b1d5d
Jonathan Hartman <j@p4nt5.com>
2016-07-29 14:37:29 -0700

    Add 'frozen_string_literal: true' to all Ruby files

--- cookbooks/code_generator/libraries/chef_dk_generator_template_helper.rb
+++ cookbooks/code_generator/libraries/chef_dk_generator_template_helper.rb
@@ -1,4 +1,5 @@
-# Encoding: UTF-8
+# encoding: utf-8
+# frozen_string_literal: true
 #
 # Cookbook Name:: code_generator
 # Library:: chef_dk_generator_template_helper
--- test/knife.rb
+++ test/knife.rb
@@ -1,4 +1,5 @@
-# Encoding: UTF-8
+# encoding: utf-8
+# frozen_string_literal: true
 
 cache_type 'BasicFile'
 cache_options(path: File.expand_path('../../.kitchen/.cache', __FILE__))
